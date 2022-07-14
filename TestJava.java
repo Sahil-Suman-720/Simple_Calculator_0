8f9162f6ce38abf361f21b348f2c7cc96b26da5b
Sahil_Suman_720 <sahilsuman2002@gmail.com>
2022-07-14 16:35:47 +0530

    Second test change

--- TestJava.java
+++ TestJava.java
@@ -3,6 +3,9 @@ public class TestJava {
     public static void main(String[] args)
     {
         System.out.print("A Java Projekt too");
+        System.out.print("Checking of version control");
+        System.out.print("If changes are reflected or not");
+        
     }
     
 }
